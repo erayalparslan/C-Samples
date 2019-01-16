426fd174f4cbe64dea4254e2b36c614a6a323cd6
Eray <a@a-MacBook-Pro.local>
2019-01-16 23:49:52 +0300

    add GetInput

---
+++ GetInput/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    int number;
+    std::cout<<"Enter your favorite number: ";
+    std::cin>>number;
+    std::cout<<"\nWow!! " << number << " is my favorite number too!" << std::endl;
+
+    return 0;
+}
