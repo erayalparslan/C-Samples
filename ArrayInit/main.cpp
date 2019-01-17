9398c9d218de9a15a12e9768337292a13a863968
Eray <a@a-MacBook-Pro.local>
2019-01-17 23:27:13 +0300

    array initialization

---
+++ ArrayInit/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numbers[10] {54,34,23,7,8,9,1,2222,1121,77};
+    int anotherArray[10] {3,6}; // remaining is 0
+    int daysArray[365] {0}; // init all to zero
+    double autoArray[] {56.34, 542.77, 122.0, 930.33};
+    
+    
+}
