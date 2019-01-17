635344ba74c837106c5ffa34fc1a88e063d8362f
Eray <a@a-MacBook-Pro.local>
2019-01-17 17:23:29 +0300

    datatype minmax values

---
+++ MinMaxValues/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main(){
+    cout << "Minimum Values: \n=========================" << endl;
+    
+    cout << "Char: "       << CHAR_MIN      << endl;
+    cout << "Int: "        << INT_MIN       << endl;
+    cout << "Short: "      << SHRT_MIN      << endl;
+    cout << "Long: "       << LONG_MIN      << endl;
+    cout << "Long Long: "  << LLONG_MIN << endl;
+    
+    
+    cout << "\n\nMaximum Values: \n=========================" << endl;
+    
+    cout << "Char: "       << CHAR_MAX      << endl;
+    cout << "Int: "        << INT_MAX       << endl;
+    cout << "Short: "      << SHRT_MAX      << endl;
+    cout << "Long: "       << LONG_MAX      << endl;
+    cout << "Long Long: "  << LLONG_MAX << endl;
+    
+    
+    
+}
