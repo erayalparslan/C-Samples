8056bbcecc017fe5e458f0c1968048f9fd31f8a3
Eray <a@a-MacBook-Pro.local>
2019-01-17 14:01:21 +0300

    using namespace

---
+++ GetInput/main2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+int main(){
+    
+    int number;
+    
+    cout << "What is your favorite number: ";
+    cin >> number;
+    cout<<"\nWow!! " << number << " is my favorite number too!" << endl;
+
+    return 0;
+}
