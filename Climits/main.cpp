bfaf107cf21221b56bbbe1a2f53fd25fa0b7075a
Eray Alparslan <erayalparslan@yahoo.com>
2019-04-09 00:44:41 +0300

    climit header file

---
+++ Climits/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <climits>
+
+int main()
+{
+
+    cout << "Number of bits in a char is: " << CHAR_BIT << endl;
+    cout << "Max Char value is:  " << CHAR_MAX << endl;
+    cout << "Min Char value is:  " << CHAR_MIN << endl;
+    cout << "Max Signed Char value is:  " << SCHAR_MAX << endl;
+    cout << "Min Signed Char value is:  " << SCHAR_MIN << endl;
+    cout << "Max Unsigned Char value is:  " << UCHAR_MAX << endl;
+    cout << "Max Short value is:  " << SHRT_MAX << endl;
+    cout << "Min Short value is:  " << SHRT_MIN << endl;
+    cout << "Max Unsigned short value is:  " << USHRT_MAX << endl;
+    cout << "Max int value is:  " << INT_MAX << endl;
+    cout << "Min int value is:  " << INT_MIN << endl;
+    cout << "Max Unsigned int value is:  " << UINT_MAX << endl;
+    cout << "Max long value is:  " << LONG_MAX << endl;
+    cout << "Min long value is:  " << LONG_MIN << endl;
+    cout << "Max Unsigned long value is:  " << ULONG_MAX << endl;
+    cout << "Max long long value is:  " << LLONG_MAX << endl;
+    cout << "Min long long value is:  " << LLONG_MIN << endl;
+    cout << "Max Unsigned long long value is:  " << ULLONG_MAX << endl;
+
+    return 0;
+}
