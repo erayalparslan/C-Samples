1e980d6e6ef0d36b54e359e40b51cab6e0d2ad34
Eray Alparslan <erayalparslan@yahoo.com>
2019-04-09 01:33:03 +0300

    Integer Literals

--- Integer Literals/main.cpp
+++ Integer Literals/main.cpp
@@ -11,10 +11,24 @@ using namespace std;
 int main()
 {
 
+    // Different base to decimal
+
+
     int a = 42, b = 0x42, c = 042;
-    cout << "Decimal 42 is: " << a << endl;	// 42
-    cout << "Hexadecimal 42 is: " << b << endl; // 66
-    cout << "Octal 42 is: " << c << endl;	// 34
+    cout << "Decimal 42 is: " << a << endl;
+    cout << "Hexadecimal 42 is: " << b << endl;
+    cout << "Octal 42 is: " << c << endl;
+    cout << "\n************************" << endl;
+
+    // Decimal base to different base
+
+    int d = 42;
+
+    cout << "Decimal 42 is: " << d << endl;
+    cout << hex;  // Manipulator for changing base
+    cout << "Hex 42 is: " << d << endl;
+    cout << oct;  // Manipulator for changing base
+    cout << "Octal 42 is: " << d << endl;
 
     return 0;
 }
